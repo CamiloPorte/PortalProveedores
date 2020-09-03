abdd769d64e8914ebfd0690d377f3078dc6cc81a
Andres Hiran <andrescomte@gmail.com>
2020-09-03 11:43:30 -0400

    upload file v1

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ sys.path.append(app_path)
 from app import app
 from forms import *
 from flask import render_template,request,redirect, make_response, session, escape, url_for, flash
+from werkzeug.utils import secure_filename
 
 
 ###############################################################
@@ -16,14 +17,29 @@ from flask import render_template,request,redirect, make_response, session, esca
 def inicio():
 	return render_template("login.html")
 
-<<<<<<< HEAD
-@app.route('/index', methods=["POST", "GET"])
-def index():
-	return render_template("index.html")
-=======
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
 	return render_template("register.html")
+
+@app.route('/formulario', methods = ["POST", "GET"])
+def formulario():
+	return render_template("index2.html")
+
+@app.route('/upload', methods = ["POST", "GET"])
+def uploadsd():
+	if request.method == "POST":
+		file = request.files["file"]
+		file.save(os.path.join("uploads", file.filename))
+		return render_template("index2.html", message = "success")
+	return render_template("index2.html", message = "Upload")
+
+
+
+##@app.route('/upload', methods = ["POST", "GET"])
+##def uploader():
+	##if request.methods == 'POST':
+	##	f = request.form['excel']
+
 
 
 ###############################################################
@@ -31,4 +47,3 @@ def signup():
 #						VISTAS VENDERDOR	     			  #
 #															  #
 ###############################################################
->>>>>>> 1d020ff6ec331a5191432194b1da81e712fa3305
